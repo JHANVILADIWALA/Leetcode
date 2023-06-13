5af825eb1a0278402bf382bdd6a77bda8ff5f72c
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-13 17:24:47 +0530

    Time: 106 ms (71.56%), Space: 32.6 MB (61.06%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         //storing rows count in map
+        
         map< vector<int> , int > m;
         for(int i=0 ; i<grid.size() ; i++ ){
             m[grid[i]]++;
@@ -10,7 +11,6 @@ public:
         for(int i=0 ; i<grid[0].size() ; i++ ){
             //getting column in a vector 
             vector<int>v;
-            
             for(int j=0;j<grid.size();j++){
                 v.push_back(grid[j][i]);
             }
