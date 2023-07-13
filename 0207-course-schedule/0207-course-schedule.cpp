452296cbd1a9048c9fe8dae9a740303148f7935c
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-07-13 15:01:42 +0530

    Time: 30 ms (28.81%), Space: 13.5 MB (72.62%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& v) {
+        
+        vector<int>adj[n]; //adjacency matrix
+        //creating graph
+        for(auto it:v ){
+            adj[it[0]].push_back(it[1]); // directed edge
+        }
+        
+        //check for toposort
+        //that is whether cycle present or not
+        //if present -> no not possible
+        // not present ->possible
+        
+        // BFS topological sort
+        int indegree[2000] = {0};
+        for( int i=0; i<n; i++){ //traversing all nodes
+            for(auto it: adj[i]){ //neighbours
+                indegree[it]++; //increase indegree of neighbours
+            }
+        }
+        //getting all nodes with indegree 0 in queue because they start the queue
+        queue<int>q;
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0)q.push(i); //bz 0 based nodes numbered
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node); //if node is in q then push in topo vector
+            
+            for(auto it : adj[node]){ //neighbours
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        
+        //if ordering contains all nodes -> yes
+        if(topo.size()==n)return true;
+        return false;
+    }
+};
