0ea0dfc7f2adaacf74feefc64e00255d5befdbf8
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-07-31 23:17:24 +0530

    Time: 6 ms (100.00%), Space: 7 MB (87.50%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -23,7 +23,7 @@ public:
         {
             for(int j=1;j<=m;j++)
             {
-                if(s1[i-1]==s2[j-1])
+                if(s1[i-1]==s2[j-1]) ////////////////instead of 1 we are adding s1[i-1] 
                     dp[i][j]=s1[i-1]+dp[i-1][j-1];
                 else
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
