da1a1d43c9f74a5bec70e99c907b9d66970cd0e4
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-10-23 12:23:57 +0530

    Time: 4 ms (22.26%), Space: 6.3 MB (26.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0)return false;
-        while(n%4==0){
-            n/=4;
-        }
-        if(n==1)return true;
+        
+//         bool ispowerof2=false;//condition 1
+//         if((n&(n-1))==0)ispowerof2=true;
+        
+//         bool NminusOneisDivby3=false;
+//         if( (n-1)%3==0 )NminusOneisDivby3=true;
+        
+        if((n&(n-1))==0 && (n-1)%3==0)return true;
         else return false;
+        
     }
 };
