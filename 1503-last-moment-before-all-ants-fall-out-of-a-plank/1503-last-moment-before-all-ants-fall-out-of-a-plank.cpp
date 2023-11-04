fafa8bc1c113887199a63820b366c4e479e6e1e6
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-11-04 12:57:25 +0530

    Time: 28 ms (6.90%), Space: 23.1 MB (31.47%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        //we assume that ants can cross each other
+        //so the time they will take is same as they would have taken by colliding
         int l=0, r=0;
         sort(left.begin(), left.end());
         sort(right.begin(), right.end());
