02cdc77e9c25ef6dfdef82b57cd70e55c7195f04
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2024-01-21 10:51:30 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,9 +3,8 @@ public:
     int fn(vector<int>nums, int i, vector<int>&dp){
         if(i<0)return 0;
         if(dp[i]!=-1)return dp[i];
-        int rob= nums[i] + fn(nums, i-2, dp);
-        int dont= fn(nums,i-1,dp);
-        return dp[i]=max(rob, dont);
+        
+        return dp[i]=max(nums[i]+fn(nums, i-2, dp), fn(nums, i-1, dp));
     }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size(), -1);
